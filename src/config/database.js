2b6cc41894b39f2c270bf13d38a1aae6d6f9c091
Umemura <roberto.silva@novaandradina.org>
2020-04-26 21:35:54 -0400

    novo

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: "postgres",
+  host: "motty.db.elephantsql.com",
+  username: "zpofizho",
+  password: "MwjmDhbSCKeqUjKxVhePrcbdpanRCELD",
+  database: "zpofizho",
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
